266130546944fcff2f2cc5c63cb7262c78937a28
ThisZW <everbread1234@gmail.com>
2018-03-12 01:38:22 -0400

    account db tested and created.

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class account(models.Model):
+	class Meta:
+		db_table = "account"
+	account_id = models.IntegerField(default=10000)
+	first_name = models.CharField(max_length=12)
+	last_name = models.CharField(max_length=12)
+	username = models.CharField(max_length=30)
+	password = models.CharField(max_length=30)
+	email = models.CharField(max_length=50)
+	time_registered = models.DateTimeField(auto_now_add=True)
+	time_last_login = models.DateTimeField(auto_now_add=True)
+	ACC_TYPES = (
+		('ss','store superuser'),
+		('sc','store cook'),
+		('dm','delivery man'),
+		('fc','food customer'),	
+	)
+	account_type = models.CharField(max_length=2, choices = ACC_TYPES)
+
