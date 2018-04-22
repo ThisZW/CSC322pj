e0bf6c8badf5ee28bb61e7caab2d9d47c727a7c5
ThisZW <everbread1234@gmail.com>
2018-04-22 01:02:23 -0400

    account page model/ checkout, setorder model complete

---
+++ resources/views/checkout/success.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.layout')
+
+
+@section('front-content')
+<div class="container">
+
+	<h3>
+		Congratulations!<br>
+	</h3>
+	<h4>
+	You have successfully placed the order, check your account page for details. </h4>
+
+</div>
+@endsection
