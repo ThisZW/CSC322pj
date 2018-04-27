af3af98307748de82022e0f87cf35458a2c68f4d
ThisZW <everbread1234@gmail.com>
2018-04-26 23:33:16 -0400

    rating model/controller init.

---
+++ database/migrations/2018_04_25_215418_create_delivery_ratings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveryRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('delivery_id');
+            $table->float('score',2);
+            $table->integer('rater_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_ratings');
+    }
+}
