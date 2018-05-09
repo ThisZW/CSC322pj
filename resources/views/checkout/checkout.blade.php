90a3e5af8cc252e63cb6bd78609f77b00ab0add2
ThisZw <everbread1234@gmail.com>
2018-05-09 04:06:02 -0400

    will work on paypal

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -1,5 +1,45 @@
-@include('layouts.header')
+@extends('layouts.layout')
 
+@section('header-extra')
+<script src="https://www.paypalobjects.com/api/checkout.js"></script>
+  <script>
+    paypal.Button.render({
+      env: 'production', // Or 'sandbox',
+
+      commit: true, // Show a 'Pay Now' button
+
+      style: {
+        color: 'gold',
+        size: 'small'
+      },
+
+      payment: function(data, actions) {
+        /* 
+         * Set up the payment here 
+         */
+      },
+
+      onAuthorize: function(data, actions) {
+        /* 
+         * Execute the payment here 
+         */
+      },
+
+      onCancel: function(data, actions) {
+        /* 
+         * Buyer cancelled the payment 
+         */
+      },
+
+      onError: function(err) {
+        /* 
+         * An error occurred during the transaction 
+         */
+      }
+    }, '#paypal-button');
+  </script>
+@endsection
+@section('content')
 <div class="checkout-container main">
  
 <div class="row">
@@ -93,5 +133,4 @@ Cook/Superuser page-> .......
 
 
 -->
-@include('layouts.footer')
-<pre>{{ print_r(session()->all())}}</pre>
+@endsection
